b6d26c36dde2bb7a1b0c04793cbe2857e5326f96
inespr <iperrom@g.educaand.es>
2023-03-19 20:40:35 +0100

    fix input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,13 +5,12 @@ export function Input(props) {
   return (
     <div className="inside-input">
       <input
-      type={props.type}
-      placeholder={props.placeholder}
-      className={props.className}
-      id={props.id}
-      onChange={props.onChange}
-
-      />    </div>
-
+        type={props.type}
+        placeholder={props.placeholder}
+        className={props.className}
+        id={props.id}
+        ref={props.reference}
+      />
+    </div>
   );
 }
