b75499a752faa965c205af198294fedc2d111404
inespr <iperrom@g.educaand.es>
2023-03-20 14:09:26 +0100

    Create user list map

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,24 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { FiLogOut, FiUser } from "react-icons/fi";
-import { Link } from 'react-router-dom';
-import { User } from './User';
-
+import { Link } from "react-router-dom";
+import { User } from "./User";
+import "../style/UsersList.scss";
 
 export function UsersList() {
+  const [data, setData] = useState();
+  const bearer = localStorage.getItem("Token");
+  console.log(bearer);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${bearer}`,
+      },
+    };
+    fetch(`${import.meta.env.VITE_APP_MY_API_LINK}/users`, options)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.items);
+        console.log(data);
+        console.log(data.items);
+      });
+  }, []);
+
   return (
-    <>
-    <section className='users--header'>
-      <div className='userLogger'>
-      <FiUser/>
-      <span>GL</span>
+    <section className="usersList">
+      <section className="users--header">
+        <div className="user--header--wrapper">
+          <button className="userLogger">
+            <span>GL</span>
+          </button>
+          <div className="">
+            <Link to="../log-out" className="logOut">
+              <FiLogOut />
+            </Link>
+          </div>
+        </div>
+      </section>
+      <div className="userslist">
+        <h1 className="tittle">Users List</h1>
+        <section className="Users">
+          {data &&
+            data.map((element, index) => {
+              console.log(data.items);
+              console.log(element.email);
+
+              return <User key={index} userMail={element.email} />;
+            })}
+        </section>
       </div>
-      <Link to='../log-out' className='logOut'><FiLogOut/></Link>
     </section>
-    <div className="userslist">
-      <h1 className='tittle'>Users List</h1>
-      <User/>
-    </div>
-    </>
-    
   );
 }
