2b6dd2d1323d019d1ea56aea4d67c99d84ed52c2
inespr <iperrom@g.educaand.es>
2023-03-21 14:15:04 +0100

    Create show menu, delete option, show edit

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,53 +1,104 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {
   FiUserX,
   FiEdit,
   FiInfo,
   FiMoreVertical,
-  FiMoreHorizontal,
   FiCornerDownLeft,
 } from "react-icons/fi";
 import "../style/User.scss";
+import { UserInfo } from "./UserInfo";
 
-export function User({ userMail }) {
+export function User({
+  userMail,
+  userName,
+  userSurname,
+  userId,
+  handleUserEvent,
+}) {
+  const [activeMenu, setActiveMenu] = useState(false);
+  const [deleteMenu, setDeleteMenu] = useState(false);
+  const DataUser = {
+    userId,
+    userName ,
+    userSurname,
+    userMail,
+  };
+
+  const bearer = localStorage.getItem("Token");
+  console.log(userId);
+
   function showMenu() {
-    
+    setActiveMenu(!activeMenu);
   }
+  function showDeteleMenu() {
+    setDeleteMenu(!deleteMenu);
+  }
+  function deleteUser() {
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${bearer}`,
+      },
+    };
+    fetch(
+      `${import.meta.env.VITE_APP_MY_API_LINK}/users/${userId}`,
+      options
+    ).catch((error) => console.error("Error fetching DataUser: ", error));
+
+    handleUserEvent("delete");
+    setDeleteMenu(!deleteMenu);
+    setActiveMenu(!activeMenu);
+  }
+
   return (
-    <div className="user">
-      <button className="user--div">
+    <div className="user" id="user">
+      <button className="user--div" onClick={showMenu}>
         <span className="userMail">{userMail}</span>
-        <FiMoreVertical className="moreVertical" />
+        <FiMoreVertical
+          className={
+            activeMenu ? "moreVertical moreHorizontal" : "moreVertical"
+          }
+        />
       </button>
-      <div className="user--menu">
-        <span className="userMail">{userMail}</span>
-        <FiMoreHorizontal className="moreHorizontal" />
-        <section className="menu--icon">
-          <FiUserX />
-          <Link to="../user-edit">
-            <FiEdit />
-          </Link>
-          <Link to="../user-info">
-            <FiInfo />
-          </Link>
-        </section>
-      </div>
-      <div className="user--delete--confirmation">
-        <section className="delete">
-          <button className="delete--button">
-            <span className="message--sure">Are you sure?</span>
-            <span className="message--delete">DELETE USER</span>
-            <FiUserX className="icon--delete" />
-          </button>
-        </section>
-        <section className="return">
-          <button className="return--button">
-            <span>RETURN</span>
-            <FiCornerDownLeft className="return--icon" />
-          </button>
-        </section>
-      </div>
+      {activeMenu &&
+        (deleteMenu ? (
+          <div className="user--delete--confirmation">
+            <section className="delete">
+              <button className="delete--button" onClick={deleteUser}>
+                <span className="message--sure">Are you sure?</span>
+                <span className="message--delete">DELETE USER</span>
+                <FiUserX className="icon--delete" />
+              </button>
+            </section>
+            <section className="return">
+              <button className="return--button">
+                <span onClick={showDeteleMenu}>RETURN</span>
+                <FiCornerDownLeft className="return--icon" />
+              </button>
+            </section>
+          </div>
+        ) : (
+          <div className="user--menu" id="user--menu">
+            <section className="menu--icon">
+              <button onClick={showDeteleMenu} className="delete">
+                <FiUserX />
+              </button>
+              <Link
+                to="../user-edit"
+                className="edit"
+                onClick={localStorage.setItem("Data", JSON.stringify(DataUser))}
+              >
+                <FiEdit />
+              </Link>
+              <Link to={`../user-info`} className="info">
+                <FiInfo />
+              </Link>
+            </section>
+          </div>
+        ))}
     </div>
   );
 }
