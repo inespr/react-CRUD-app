e8e73c71dd16f036fd8bd278eea957f00a706b77
inespr <iperrom@g.educaand.es>
2023-03-21 14:16:06 +0100

    Add user information to userInfo

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -3,7 +3,23 @@ import { Link } from "react-router-dom";
 import { FiCornerDownLeft, FiInfo } from "react-icons/fi";
 import { FormLayout } from "../layout/FormLayout";
 
-export function UserInfo({ name, surname, email, password }) {
+export function UserInfo() {
+    const bearer = localStorage.getItem("Token");
+    const dataUser = JSON.parse(localStorage.getItem("Data"));
+
+    console.log(dataUser)
+
+  const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${bearer}`,
+      },
+    };
+    fetch(`${import.meta.env.VITE_APP_MY_API_LINK}/users/${dataUser.userId}`, options)
+    
+    .catch((error) => console.error("Error fetching data: ", error));
+
   return (
     <div className="Form">
       <FormLayout>
@@ -14,16 +30,13 @@ export function UserInfo({ name, surname, email, password }) {
             <FiCornerDownLeft />
           </Link>
           <div type={"text"} className="input">
-            Name...
+           {dataUser.userName}
           </div>
           <div type={"text"} className="input">
-            Surname...
+          {dataUser.userSurname}
           </div>
           <div type={"email"} className="input">
-            Email...
-          </div>
-          <div type={"password"} placeholder="" className="input">
-            Password...
+          {dataUser.userMail}
           </div>
         </form>
       </FormLayout>
