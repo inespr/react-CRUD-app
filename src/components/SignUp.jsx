d1dcd3d176263bfa526a244d7323b4d8db193b66
inespr <iperrom@g.educaand.es>
2023-03-22 13:10:33 +0100

    Add react-toastify to confirm create user

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,8 +2,13 @@ import React, { useEffect, useRef, useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 import { FormLayout } from "../layout/FormLayout";
 import { Input } from "./Input";
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+
 
 export function SignUp() {
+
   const nameRef = useRef();
   const surnameRef = useRef();
   const emailRef = useRef();
@@ -13,6 +18,8 @@ export function SignUp() {
   const [status, setStatus] = useState(" ");
 
   console.log("rendered once");
+  const notify = () => toast.success("User create!");
+
 
   function callApi(event) {
     event.preventDefault();
@@ -46,7 +53,7 @@ export function SignUp() {
             } else if (response.statusCode === 500) {
               console.log(response.statusCode);
               throw 500;
-            } 
+            }
             setError('');
           })
           .then((data) => {
@@ -68,7 +75,9 @@ export function SignUp() {
     setError("");
   }, [status]);
   return (
+    
     <div className="Form">
+      <ToastContainer />
       <FormLayout>
         <form onSubmit={callApi} >
           <h1>SignUp</h1>
@@ -118,7 +127,7 @@ export function SignUp() {
                 <span className="button--error--name">{error}</span>
               </button>
             )}
-            <button type="submit" className="button--continue">
+            <button type="submit" className="button--continue" onClick={notify}>
               <span className="button--name">CONTINUE</span>
             </button>
           </div>
