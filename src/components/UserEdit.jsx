1483cb35fdcc0a379b9785c43d931091b4369a91
inespr <iperrom@g.educaand.es>
2023-03-22 10:35:07 +0100

    Create and style user edit

--- src/components/UserEdit.jsx
+++ src/components/UserEdit.jsx
@@ -1,28 +1,95 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiCornerDownLeft, FiCheck, FiEdit } from "react-icons/fi";
 import { FormLayout } from "../layout/FormLayout";
 
-export function UserEdit({ name, surname, email, password }) {
+export function UserEdit() {
+  const [name, setName] = useState();
+  const [surname, setSurname] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const bearer = localStorage.getItem("Token");
+  const dataUser = JSON.parse(localStorage.getItem("Data"));
+
+  useEffect(() => {
+    setName(dataUser.userName);
+    setSurname(dataUser.userSurname);
+    setEmail(dataUser.userMail);
+  }, []);
+
+  function callApi() {
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${bearer}`,
+      },
+      body: JSON.stringify({
+        name: name,
+        surname: surname,
+        email: email,
+        password: password,
+      }),
+    };
+    fetch(
+      `${import.meta.env.VITE_APP_MY_API_LINK}/users/${dataUser.userId}`,
+      options
+    ).catch((error) => console.error("Error fetching data: ", error));
+  }
+
   return (
     <div className="Form">
       <FormLayout>
-        <form>
-          <h1>UserEdit</h1>
-          <FiEdit />
-          <Link to="../users-list">
+        <form onSubmit={callApi} className="form">
+          <Link to="../users" className="return">
             <FiCornerDownLeft />
           </Link>
-          <input type={"text"} placeholder="Name..." className="input" />
-          <input type={"text"} placeholder="Surname..." className="input" />
-          <input type={"email"} placeholder="Email..." className="input" />
-          <input
-            type={"password"}
-            placeholder="Password..."
-            className="input"
-          />
+          <div className="header">
+            <h1>UserEdit</h1>
+            <FiEdit />
+          </div>
+
+          <label>
+            Name:
+            <input
+              type={"text"}
+              placeholder="Name..."
+              className="input"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+          </label>
+          <label>
+            Surname:
+            <input
+              type={"text"}
+              placeholder="Surname..."
+              className="input"
+              value={surname}
+              onChange={(event) => setSurname(event.target.value)}
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type={"email"}
+              placeholder="Email..."
+              className="input"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type={"password"}
+              placeholder="Insert a new password"
+              className="input"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
           <button type={"submit"} className="button--save">
-            SAVE <FiCheck />
+            <span className="button--name">SAVE</span> <FiCheck />
           </button>
         </form>
       </FormLayout>
