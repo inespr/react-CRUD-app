a7f43b4c2d0c2b24d177dffed0db67e0399663f1
inespr <iperrom@g.educaand.es>
2023-03-22 10:35:29 +0100

    Create and style user Info

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -2,42 +2,57 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FiCornerDownLeft, FiInfo } from "react-icons/fi";
 import { FormLayout } from "../layout/FormLayout";
+import "../style/UsersList.scss";
+import "../style/User.scss";
 
 export function UserInfo() {
-    const bearer = localStorage.getItem("Token");
-    const dataUser = JSON.parse(localStorage.getItem("Data"));
+  const bearer = localStorage.getItem("Token");
+  const dataUser = JSON.parse(localStorage.getItem("Data"));
 
-    console.log(dataUser)
+  console.log(dataUser);
 
   const options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${bearer}`,
-      },
-    };
-    fetch(`${import.meta.env.VITE_APP_MY_API_LINK}/users/${dataUser.userId}`, options)
-    
-    .catch((error) => console.error("Error fetching data: ", error));
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${bearer}`,
+    },
+  };
+  fetch(
+    `${import.meta.env.VITE_APP_MY_API_LINK}/users/${dataUser.userId}`,
+    options
+  ).catch((error) => console.error("Error fetching data: ", error));
 
   return (
     <div className="Form">
       <FormLayout>
-        <form>
-          <h1>UserInfo</h1>
-          <FiInfo />
-          <Link to="../users-list">
+        <form className="form">
+        <Link to="../users" className="return">
             <FiCornerDownLeft />
           </Link>
-          <div type={"text"} className="input">
-           {dataUser.userName}
+          <div className="header">
+          <h1>UserInfo</h1>
+          <FiInfo />
           </div>
-          <div type={"text"} className="input">
-          {dataUser.userSurname}
-          </div>
-          <div type={"email"} className="input">
-          {dataUser.userMail}
-          </div>
+
+          <label>
+            Name:
+            <div type={"text"} className="input">
+              {dataUser.userName}
+            </div>
+          </label>
+          <label>
+            Surname:
+            <div type={"text"} className="input">
+              {dataUser.userSurname}
+            </div>
+          </label>
+          <label>
+            Email:
+            <div type={"email"} className="input">
+              {dataUser.userMail}
+            </div>
+          </label>
         </form>
       </FormLayout>
     </div>
