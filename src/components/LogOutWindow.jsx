bbc36af0776159618fcac16f135534c55993942e
inespr <iperrom@g.educaand.es>
2023-03-22 13:10:59 +0100

    Fix logout window

--- src/components/LogOutWindow.jsx
+++ src/components/LogOutWindow.jsx
@@ -2,20 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FiXCircle, FiCheck } from "react-icons/fi";
 
+import '../style/LogOutWindow.scss';
+
+
 export function LogOutWindow() {
   function removeUser() {
     localStorage.removeItem('Token')
     localStorage.removeItem('EmailAcces')
   }
   return (
-    <div className="LogOut">
+    <div className="logOut">
+      <div className="card">
+      <div className="closeCircle">
       <Link to="../users-list">
-        <FiXCircle className="closeCircle" />
+        <FiXCircle  />
       </Link>
+      </div>
+     
       <h1>Log Out</h1>
-      <Link to="../log-in" onClick={removeUser}>
-        <FiCheck className="check" />
+      <Link to="../log-in" onClick={removeUser} className="check">
+        <span>CONFIRM</span><FiCheck  />
       </Link>
+      </div>
+     
     </div>
   );
 }
